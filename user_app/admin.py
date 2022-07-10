feb4a931a0da15694f2562b375c6c5b4ed00cc96
soft <superstargajanikant@gmail.com>
2022-07-10 12:16:12 +0545

    changes and additions

--- user_app/admin.py
+++ user_app/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from user_app.models import Customer, User
 from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email','phone',)
     
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Customer)
