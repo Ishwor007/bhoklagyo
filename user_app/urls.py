defa96752c222278abc2e1dc5687993d8267d36c
soft <superstargajanikant@gmail.com>
2022-07-10 12:08:32 +0545

    added url paths

--- user_app/urls.py
+++ user_app/urls.py
@@ -5,6 +5,7 @@ from . import views as user_views
 urlpatterns = [
     path('signup/',user_views.signup_page, name='signup'),
     path('login/',user_views.login_page, name='login'),
+    path('logout/', user_views.logout_user, name='logout'),
     path('register/',user_views.register_customer, name='register'),
     path('authenticate/',user_views.login_user, name='authenticate'),
 ]
