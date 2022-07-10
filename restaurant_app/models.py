c0a4a789cebdb5e62d7db963836e883513a9718f
soft <superstargajanikant@gmail.com>
2022-07-10 12:11:48 +0545

    updated restaurant model fields

--- restaurant_app/models.py
+++ restaurant_app/models.py
@@ -3,18 +3,23 @@ from user_app.models import User
 
 class Restaurant(models.Model):
     restaurant_name = models.CharField(max_length=200)
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    location = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=10, unique=True)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
+    register_date = models.DateField(auto_now_add=True)
+    last_modified_on = models.DateField(auto_now=True)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.restaurant_name
 
     
-class RestaurantAdmin(models.Model):
+class RestaurantAdmin(User):
     restaurant_name = models.OneToOneField(Restaurant,on_delete=models.CASCADE,related_name='restaurant')
-    phone = models.CharField(max_length=14, unique=True)
-    email = models.EmailField(max_length=200, unique=True)
-
+    
+    role = User.RESTAURANT_ADMIN
+    
     is_superuser = False
     is_staff = True 
     is_active = True
