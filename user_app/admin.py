f367ccff0221d9c8b1e6ca01a62fb80fa6cfe49a
soft <superstargajanikant@gmail.com>
2022-07-16 10:10:01 +0545

    fixed problems on admin site

--- user_app/admin.py
+++ user_app/admin.py
@@ -3,19 +3,52 @@ from django.contrib import admin
 from user_app.models import Customer, User
 from django.contrib.auth.admin import UserAdmin
 
-# class CustomUserAdmin(UserAdmin):
-#     ordering = ('email','phone')
-#     list_display =('email','phone','role',)
-#     list_filter = ('RestaurantAdmin__is_staff','RestaurantAdmin__is_admin','RestaurantAdmin__is_active')
+class CustomUserAdmin(UserAdmin):
+    ordering = ('email','phone')
+    list_display =('email','phone','role','is_superuser','is_staff','is_active')
+    # list_filter = ('RestaurantAdmin__is_staff','RestaurantAdmin__is_admin','RestaurantAdmin__is_active')
 
-# class CustomerAdmin(UserAdmin):
-#     ordering = ('phone',)
-#     list_display = ('phone','role',)
-#     list_filter = ('Customer__is_staff','Customer__is_admin','Customer__is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password'),
+        }),
+    )
+    search_fields = ('phone',)
+    ordering = ('phone',)
+    filter_horizontal = ()
     
-# admin.site.register(User, CustomUserAdmin)
-# admin.site.register(Customer, CustomerAdmin)
+class CustomerAdmin(UserAdmin):
+    ordering = ('phone',)
+    list_display = ('phone',)
+    # list_filter = ('Customer__is_staff','Customer__is_admin','Customer__is_active')    
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password'),
+        }),
+    )
+    search_fields = ('phone',)
+    ordering = ('phone',)
+    filter_horizontal = ()
+
+    
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Customer, CustomerAdmin)
 
 
-admin.site.register(User)
-admin.site.register(Customer)
+# admin.site.register(User)
+# admin.site.register(Customer)
