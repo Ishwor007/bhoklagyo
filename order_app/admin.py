3407235c87281bcd1a2a8a5db90734a736e6fe19
unknown <superstargajanikant@gmail.com>
2022-09-17 08:24:51 +0545

    added billing location, needs fixes

--- order_app/admin.py
+++ order_app/admin.py
@@ -1,9 +1,10 @@
 from urllib.request import CacheFTPHandler
 from django.contrib import admin
 
-from .models import Order, Cart
+from .models import Order, Cart, BillingLocation
 
 admin.site.register(Order)
 admin.site.register(Cart)
+admin.site.register(BillingLocation)
 
 
